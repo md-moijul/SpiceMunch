b4554dbe1da7519b3716e688f11ba58318a31039
moijul <moijul@wilxite.com>
2023-12-24 17:50:52 +0000

    select item

--- app/src/main/java/com/example/spice_munch/activity/FoodActivity.kt
+++ app/src/main/java/com/example/spice_munch/activity/FoodActivity.kt
@@ -3,21 +3,43 @@ package com.example.spice_munch.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ListView
 import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
 import com.example.spice_munch.R
+import com.example.spice_munch.viewmodel.FoodViewModel
 
 class FoodActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: FoodViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_food)
 
-        // Get the counter value from the Intent
-        val counterValue = intent.getIntExtra("counter_value", 0)
+        viewModel = ViewModelProvider(this).get(FoodViewModel::class.java)
 
-        // Find the TextView and set the counter value
+        // Find the TextView and set the counter value from the Intent
         val counterTextView: TextView = findViewById(R.id.counterTextView)
+        val counterValue = intent.getIntExtra("counter_value", 0)
         counterTextView.text = "Table number: $counterValue"
+
+        // Setup the ListView and Adapter
+        val listView: ListView = findViewById(R.id.listViewFoodItems)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, viewModel.items)
+        listView.adapter = adapter
+
+
+        // Set the item click listener
+        listView.setOnItemClickListener { _, _, position, _ ->
+            val selectedFoodItem = viewModel.items[position]
+            val intent = Intent(this, ModificationActivity::class.java).apply {
+                putExtra("selected_item", selectedFoodItem)
+            }
+            startActivity(intent)
+        }
+
 
 
         val selectItemButton: Button = findViewById(R.id.selectItemButton)
---
+++ app/src/main/java/com/example/spice_munch/viewmodel/FoodViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.spice_munch.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class FoodViewModel : ViewModel() {
+    val items = listOf("Massala", "Korma", "Sag Alu", "Rice", "Nan")
+}
